4b500b23e7cf01d39d053524e99d971d9c0770f3
iliay038 <iliay038@su00-110.ad.liu.se>
2019-11-22 16:03:28 +0100

    started on lab2

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -15,3 +15,44 @@ DROP TABLE IF EXISTS custom_table CASCADE;
 
 SOURCE company_schema.sql;
 SOURCE company_data.sql;
+
+/*
+Question 3, 
+
+*/
+create table jbmanager(
+	id integer,
+	bonus integer default 0,
+	constraint pk_manager
+		primary key (id), 	
+	constraint fk_employee 
+		foreign key (id) references jbemployee(id)
+);
+
+/*
+We initialize the bonus value to 0, so that adding a bonus will be possible since its an integer(not NULL). Should someone want to calculate the average bonus salary it would not be possible with null as a initial value since that manager would not be included. However with 0 they would be.
+*/
+insert into jbmanager (select id,0 from jbemployee where id in (select manager from jbemployee));
+
+
+alter table jbemployee drop foreign key fk_emp_mgr;
+alter table jbemployee add constraint fk_emp_mgr foreign key (manager) references jbmanager(id)
+on delete set null;
+
+
+alter table jbdept drop foreign key fk_dept_mgr;
+alter table jbemployee add constraint fk_dept_mgr foreign key (manager) references jbmanager(id)
+on delete set null;
+
+/*
+Question 4
+*/
+
+update jbmanager
+set bonus= bonus +10000
+where id in (select manager from jbdept);
+
+
+
+
+
