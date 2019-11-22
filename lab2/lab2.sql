efef6ac9c483b2d617c35f26c587cc94e743c629
Ilian Ayoub <iliay038@student.liu.se>
2019-11-22 12:10:17 +0100

    Update lab2.sql

---
+++ lab2/lab2.sql
@@ -0,0 +1,17 @@
+/*
+Lab 2 report chrha376 iliay038
+
+All non code should be within SQL-comments /* like this */ 
+
+
+/*
+Drop all user created tables that have been created when solving the lab
+*/
+
+DROP TABLE IF EXISTS custom_table CASCADE;
+
+
+/* Have the source scripts in the file so it is easy to recreate!*/
+
+SOURCE company_schema.sql;
+SOURCE company_data.sql;
